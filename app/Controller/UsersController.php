a147212d13b891fe34d4fb9eb09112186d7dcb86
jadetulda-fdc <162071313+jadetulda-fdc@users.noreply.github.com>
2024-03-15 15:40:34 +0800

    spaces to tab

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -2,115 +2,114 @@
 
 class UsersController extends AppController {
 
-    public $components = array('RequestHandler');
+	public $components = array('RequestHandler');
 
-    public function beforeFilter() {
-        parent::beforeFilter();
-        $this->Auth->allow('register', 'logout');
+	public function beforeFilter() {
+		parent::beforeFilter();
+		$this->Auth->allow('register', 'logout');
 
-        // block access to login and register if logged in
-        if (in_array($this->action, array('login', 'register')) && $this->Auth->user())
-            return $this->redirect($this->Auth->redirectUrl());
-    }
+		// block access to login and register if logged in
+		if (in_array($this->action, array('login', 'register')) && $this->Auth->user())
+			return $this->redirect($this->Auth->redirectUrl());
+	}
 
-    public function isAuthorized() {
+	public function isAuthorized() {
+		return true;
+	}
 
-        return true;
-    }
+	public function login() {
+		if ($this->request->is('post')) {
+			if ($this->Auth->login()) {
+				$this->User->id = $this->Auth->user()['id'];
+				$this->User->set(array('last_login_time' => date('Y-m-d H:i:s')));
+				$this->User->save();
 
-    public function login() {
-        if ($this->request->is('post')) {
-            if ($this->Auth->login()) {
-                $this->User->id = $this->Auth->user()['id'];
-                $this->User->set(array('last_login_time' => date('Y-m-d H:i:s')));
-                $this->User->save();
+				return $this->redirect($this->Auth->redirectUrl());
+			} else {
+				$this->Flash->error('Your username/password entered was incorrect.', array(
+					'key' => 'loginError'
+				));
+			}
+		}
+	}
 
-                return $this->redirect($this->Auth->redirectUrl());
-            } else {
-                $this->Flash->error('Your username/password entered was incorrect.', array(
-                    'key' => 'loginError'
-                ));
-            }
-        }
-    }
+	public function index() {
+		$this->set('users', $this->User->find('all'));
+	}
 
-    public function index() {
-        $this->set('users', $this->User->find('all'));
-    }
+	public function register() {
+		if ($this->request->is('post')) {
+			$this->User->create();
 
-    public function register() {
-        if ($this->request->is('post')) {
-            $this->User->create();
+			// Create profile
+			$this->loadModel('Profile');
+			$this->Profile->create();
+			$this->Profile->set(array(
+				'name' => $this->request->data['User']['name'],
+				'profile_picture' => 'profile/profile-pic.png'
+			));
 
-            // Create profile
-            $this->loadModel('Profile');
-            $this->Profile->create();
-            $this->Profile->set(array(
-                'name' => $this->request->data['User']['name'],
-                'profile_picture' => 'profile/profile-pic.png'
-            ));
+			$isValidAll = true;
 
-            $isValidAll = true;
+			if (!$this->Profile->validates()) {
+				$this->Flash->error($this->Profile->validationErrors);
+				$isValidAll = false;
+			}
 
-            if (!$this->Profile->validates()) {
-                $this->Flash->error($this->Profile->validationErrors);
-                $isValidAll = false;
-            }
+			$this->User->set($this->request->data);
+			if (!$this->User->validates()) {
+				$this->Flash->error($this->validationErrors);
+				$isValidAll = false;
+			}
 
-            $this->User->set($this->request->data);
-            if (!$this->User->validates()) {
-                $this->Flash->error($this->validationErrors);
-                $isValidAll = false;
-            }
+			if (!$isValidAll) {
+				return false;
+			}
 
-            if (!$isValidAll) {
-                return false;
-            }
+			$this->User->save($this->request->data, array('validate' => false));
 
-            $this->User->save($this->request->data, array('validate' => false));
+			$this->request->data['User'] = array_merge(
+				$this->request->data['User'],
+				array('id' => $this->User->id)
+			);
 
-            $this->request->data['User'] = array_merge(
-                $this->request->data['User'],
-                array('id' => $this->User->id)
-            );
+			$this->Profile->set('user_id', $this->User->id);
+			$this->Profile->save();
 
-            $this->Profile->set('user_id', $this->User->id);
-            $this->Profile->save();
+			// Auto login
+			$this->Flash->success('Thank you for registering!', array(
+				'key' => 'register'
+			));
+			$user = $this->User->findById($this->User->id);
+			$this->Auth->login($user);
+			$this->render('register_success');
+		}
+	}
 
-            // Auto login
-            $this->Flash->success('Thank you for registering!', array(
-                'key' => 'register'
-            ));
-            $user = $this->User->findById($this->User->id);
-            $this->Auth->login($user);
-            $this->render('register_success');
-        }
-    }
+	public function logout() {
+		return $this->redirect($this->Auth->logout());
+	}
 
-    public function logout() {
-        return $this->redirect($this->Auth->logout());
-    }
+	// API methods
+	public function recipient_list() {
+		// $this->layout = false;
+		// $response = array(
+		//     'status' => 'failed',
+		//     'message' => 'Failed to process request.'
+		// );
 
-    // API methods
-    public function recipient_list() {
-        // $this->layout = false;
-        // $response = array(
-        //     'status' => 'failed',
-        //     'message' => 'Failed to process request.'
-        // );
+		// $result = $this->User->find('list', array(
+		//     'conditions' => array('User.id != ' => $this->Auth->user('id'))
+		// ));
 
-        // $result = $this->User->find('list', array(
-        //     'conditions' => array('User.id != ' => $this->Auth->user('id'))
-        // ));
+		// if (!empty($result)) {
+		//     $response = array('status' => 'sucess', 'data' => $result);
+		// } else {
+		//     $response['message'] = 'Found no matching data.';
+		// }
 
-        // if (!empty($result)) {
-        //     $response = array('status' => 'sucess', 'data' => $result);
-        // } else {
-        //     $response['message'] = 'Found no matching data.';
-        // }
-
-        // $this->response->type('application/json');
-        // $this->response->body(json_encode($response));
-        // return $this->response->send();
-    }
+		// $this->response->type('application/json');
+		// $this->response->body(json_encode($response));
+		// return $this->response->send();
+	}
 }
--- app/View/Elements/header.ctp
+++ app/View/Elements/header.ctp
@@ -1,28 +1,28 @@
 <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="/Messageboard">
-        <?php
-        echo $this->Html->image('messageboard.png', array(
-            'width' => 30,
-            'height' => 30
-        ));
-        ?> Message Board
-    </a>
-    <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-            <span class="nav-link" href="#">
-                <span class="text-uppercase text-white">
-                    <?php echo AuthComponent::user('Profile')['name']; ?> |
-                </span>
-                <?php
-                echo $this->Html->link(
-                    'Logout',
-                    array(
-                        'controller' => 'users',
-                        'action' => 'logout'
-                    )
-                );
-                ?>
-            </span>
-        </li>
-    </ul>
+	<a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="/Messageboard">
+		<?php
+		echo $this->Html->image('messageboard.png', array(
+			'width' => 30,
+			'height' => 30
+		));
+		?> Message Board
+	</a>
+	<ul class="navbar-nav px-3">
+		<li class="nav-item text-nowrap">
+			<span class="nav-link" href="#">
+				<span class="text-uppercase text-white">
+					<?php echo AuthComponent::user('Profile')['name']; ?> |
+				</span>
+				<?php
+				echo $this->Html->link(
+					'Logout',
+					array(
+						'controller' => 'users',
+						'action' => 'logout'
+					)
+				);
+				?>
+			</span>
+		</li>
+	</ul>
 </nav>
